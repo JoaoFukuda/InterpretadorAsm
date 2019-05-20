be9f6587f68c50c0e8dac2ceb78084a29e0a36d7
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2019-05-19 21:16:20 -0300

    Troquei o framework de AWT para Swing

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import grafico.*;
 
 class Main
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		MainFrame mainFrame = new MainFrame();
 	}
 }
--- grafico/MainFrame.java
+++ grafico/MainFrame.java
@@ -1,48 +1,41 @@
 package grafico;
 
-import java.awt.*;        // Using AWT container and component classes
-import java.awt.event.*;  // Using AWT event classes and listener interfaces
-// An AWT program inherits from the top-level container java.awt.Frame
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainFrame extends Frame implements ActionListener {
-	private Label lblCount;    // Declare a Label component 
-	private TextField tfCount; // Declare a TextField component 
-	private Button btnCount;   // Declare a Button component
-	private int count = 0;     // Counter's value
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
-	// Constructor to setup GUI components and event handlers
-	public MainFrame () {
-		setLayout(new FlowLayout());
-			// "super" Frame, which is a Container, sets its layout to FlowLayout to arrange
-            // the components from left-to-right, and flow to next row from top-to-bottom.
+public class MainFrame
+{
+	private JFrame f;
+	private JTextArea textArea;
+	private JButton button;
 
-		lblCount = new Label("Counter");  // construct the Label component
-		add(lblCount);                    // "super" Frame container adds Label component
+	public MainFrame()
+	{
+		f = new JFrame("A JFrame");
+		f.setSize(250, 250);
+		f.setLocation(300,200);
 
-		tfCount = new TextField(count + "", 10); // construct the TextField component with initial text
-		tfCount.setEditable(false);       // set to read-only
-		add(tfCount);                     // "super" Frame container adds TextField component
+		textArea = new JTextArea(10, 40);
+		f.getContentPane().add(BorderLayout.CENTER, textArea);
+		
+		button = new JButton("Click Me");
+		f.getContentPane().add(BorderLayout.SOUTH, button);
 
-		btnCount = new Button("Count");   // construct the Button component
-		add(btnCount);                    // "super" Frame container adds Button component
+		button.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				textArea.append("Button was clicked\n");
 
-		btnCount.addActionListener(this);
-			// "btnCount" is the source object that fires an ActionEvent when clicked.
-			// The source add "this" instance as an ActionEvent listener, which provides
-			//   an ActionEvent handler called actionPerformed().
-			// Clicking "btnCount" invokes actionPerformed().
+			}
+		});
 
-		setTitle("Interpretador Assembly");  // "super" Frame sets its title
-        setSize(250, 100);        // "super" Frame sets its initial window size
-        
-		setVisible(true);         // "super" Frame shows
-	}
-
-	// ActionEvent handler - Called back upon button-click.
-	@Override
-	public void actionPerformed(ActionEvent evt) {
-		++count; // Increase the counter value
-		// Display the counter value on the TextField tfCount
-		tfCount.setText(count + ""); // Convert int to String
+		f.setVisible(true);
 	}
 }
