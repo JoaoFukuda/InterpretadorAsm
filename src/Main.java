f66b3571093c3785ab36504605f10d9270ae3299
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2019-05-29 21:10:18 -0300

    Cu -> Uc, adicionei um Compilador e modifiquei o Hexa * Modifiquei o nome de CU (Control Unit) para UC (Unidade de Controle) * Coloquei tudo em uma pasta cpn (ComPoNenes) * O MainFrame agora não tem nada, adicionar depois conforme necessário * Main printa um "Test" para testar se o código continua depois que a janela é criada (sim, funciona)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+// O ponto de entrada do código
 package src;
 
 import src.grafico.*;
-import src.cu.*;
+import src.cpn.*;
 
 class Main {
-	static Cu cu = new CU();
+	static Uc uc = new Uc();
 
 	public static void main(String[] args) {
-		System.out.print("Isa esteve aqui!!!!");
 		MainFrame mainFrame = new MainFrame();
+
+		System.out.println("Test");
 	}
 }
--- src/aux/Hexa.java
+++ src/aux/Hexa.java
@@ -1,4 +1,5 @@
-package src.auxiliar;
+// Guarda o hexadecimal (pensando em trocar para palavra ao invés de somente 8 bits)
+package src.aux;
 
 public class Hexa {
     private static char[] hexachar = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
