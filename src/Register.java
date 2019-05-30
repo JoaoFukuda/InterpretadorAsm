6b31bf17bba6bc0f62350d7dc319fb3f0227934d
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2019-05-30 15:28:37 -0300

    Comecei a implementar a Unidade de Controle

--- src/Register.java
+++ src/Register.java
@@ -1,11 +1,14 @@
 package src;
+
 public class Register{
     private int data;
     private int doorNumber;
+
     public Register(int data, int doorNumber){
         this.data = data;
         this.doorNumber = doorNumber;
     }
+    
     public int getData(){
         return this.data;
     }
--- src/cpn/Uc.java
+++ src/cpn/Uc.java
@@ -1,8 +1,13 @@
 // Serve para simular a Unidade de Controle e seus comandos
 package src.cpn;
 
-import src.aux.*;
+import src.*;
 
 public class Uc {
-    public Uc() { }
+    boolean[] flags;
+    Register car, cbr;
+
+    public Uc() {
+        flags = new boolean[3]; // Inicializa as 3 flags necessárias: SINAL, IGUAL A 0 e ERRO
+    }
 }
