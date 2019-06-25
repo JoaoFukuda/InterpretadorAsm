c8393d2d5f51791193a052fd51ba1aabdf2ee1c9
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2019-06-25 20:15:28 -0300

    Aux -> Auxi

--- src/aux/Compilator.java
+++
@@ -1,7 +0,0 @@
-// Pega o código do professor e transforma em opcodes para ser interpretado pelo programa
-// Utiliza somente métidos static
-package src.aux;
-
-public class Compilator {
-    static void compilar() {}
-}
